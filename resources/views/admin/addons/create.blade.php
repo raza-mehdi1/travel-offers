7c1c8fd8ebfde632139ab7f2cff495ec7456bcca
dev4 <dev4@shayansolutions.com>
2020-02-14 01:10:05 +0500

    changed page title

--- resources/views/admin/addons/create.blade.php
+++ resources/views/admin/addons/create.blade.php
@@ -32,11 +32,11 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Create Package Include</h1>
+                        <h1>Create Addon</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Package Includes</a></li>
+                            <li class="breadcrumb-item"><a href="#">Addons</a></li>
                             <li class="breadcrumb-item active">Create</li>
                         </ol>
                     </div>
